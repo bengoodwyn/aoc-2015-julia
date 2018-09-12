d3243a245e0c8ad1a17a7c250ec23ee2a8e9b77a
Ben Goodwyn <bengoodwyn@me.com>
2018-09-12 17:49:04 -0500

    :new: Day 1 Part 2

--- 1.jl
+++ 1.jl
@@ -18,7 +18,7 @@ day1_part1 = function(x)
     return floor
 end
 
-@testset "day1" begin
+@testset "day1_part1" begin
     @test 0 == day1_part1("")
     @test 1 == day1_part1("(")
     @test 2 == day1_part1("((")
@@ -35,4 +35,21 @@ end
     @test 0 == day1_part1("()\n")
 end
 
+day1_part2 = function(x)
+    str = ""
+    for c in x
+        str *= string(c)
+        if -1 == day1_part1(str)
+            break
+        end
+    end
+    return length(str)
+end
+
+@testset "day1_part2" begin
+    @test 1 == day1_part2(")")
+    @test 5 == day1_part2("()())")
+end
+
 println("""Day 1, Part 1: $(day1_part1(read("1.txt",String)))""")
+println("""Day 1, Part 2: $(day1_part2(read("1.txt",String)))""")
